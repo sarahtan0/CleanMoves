53d59f4b0e452a1228f5ab4a03ec98618cad88f6
Sarah <tans9@uci.edu>
2024-07-01 22:57:06 -0700

    removed video recorder temporarily

--- src/components/Record/Record.tsx
+++ src/components/Record/Record.tsx
@@ -1,13 +1,9 @@
 import cn from "./Record.module.css"
-import VideoRecorder from 'react-video-recorder'
 
 export function Record(){
     return (
         <div>
             <div className={cn.recorder}>
-                <VideoRecorder
-                    
-                />
             </div>
         </div>
     );
