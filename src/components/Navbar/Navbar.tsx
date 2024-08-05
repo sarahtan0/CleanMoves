431c36e29def9da91fa5fa095451e9c1acaeb0e5
Sarah <tans9@uci.edu>
2024-08-05 16:09:37 -0700

    added a new help module for the practice screen

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,24 @@
 import nav from "./Navbar.module.css"
-import {NavLink} from 'react-router-dom'
+import {NavLink, useLocation} from 'react-router-dom'
+import {useState, useEffect} from "react"
+import {PracticeHelp} from "../Help/PracticeHelp"
 
 export function Navbar(){
+    const [currPage, setCurrPage] = useState("");
+    const [pracHelpOpen, setPracHelpOpen] = useState(false);
+    let location = useLocation();
     
+    useEffect(() => {
+        setCurrPage(location.pathname);
+    }, [location])
+
+    const help = () => {
+        if(currPage == "/practice"){
+            setPracHelpOpen(!pracHelpOpen);
+        }
+        //add another for record screen
+    }
+
     return (
         <div>
             <div className={nav.navBar}>
@@ -14,8 +30,13 @@ export function Navbar(){
                     <li>
                         <NavLink to="/record">Record</NavLink>
                     </li>
+                    <li>
+                        <a onClick={help}>Help</a>
+                    </li>
                 </ul>
             </div>
+            {pracHelpOpen && 
+                <PracticeHelp />}
         </div>
     );
 }
