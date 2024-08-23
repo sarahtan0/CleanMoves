8e030c1b962a37bece42fc0df5ba7d8f1083b5ff
Sarah <tans9@uci.edu>
2024-08-23 16:01:41 -0700

    different color when the tab is selected vs not selected

--- src/components/Help/PracticeHelp.tsx
+++ src/components/Help/PracticeHelp.tsx
@@ -1,17 +1,26 @@
 import cn from "./PracticeHelp.module.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import {useState} from 'react';
 
 export function PracticeHelp(){
+    const [currTab, setCurrTab] = useState('func');
+
+    const checkTab = (val: string) => {
+        return currTab == val;
+    }
+
     return(
        <div className={cn.background}>
         <div className={cn.container}>
             <div className={cn.tabs}>
-                <button id={cn.closeBtn}>
-                    <FontAwesomeIcon icon={faXmark}/>
-                </button>
-                <div> Functions </div>
-                <div> Keybinds </div>
+                    <button id={cn.closeBtn}>
+                        <FontAwesomeIcon icon={faXmark}/>
+                    </button>
+                    <div className={(checkTab('func') ? cn.selected : cn.unselected)}
+                    onClick={() => setCurrTab('func')}> Functions </div>
+                    <div className={(checkTab('keybind') ? cn.selected : cn.unselected)}
+                    onClick={() => setCurrTab('keybind')}> Keybinds </div>
             </div>
 
             <div className={cn.content}>
