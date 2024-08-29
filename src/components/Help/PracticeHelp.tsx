c16c5f0aa877e28ca8e3034ba6d50aaf3bd4cf5d
Sarah <tans9@uci.edu>
2024-08-29 18:46:39 -0400

    added pictures and reformated descriptions

--- src/components/Help/PracticeHelp.tsx
+++ src/components/Help/PracticeHelp.tsx
@@ -25,14 +25,15 @@ export function PracticeHelp({setOpen}: PracticeHelpProps){
             <Accordion 
                 disableGutters
                 sx = {{
-                    boxShadow: 'none'
+                    boxShadow: 'none',
                 }}
             >
                 <AccordionSummary
                     expandIcon={<ArrowDropDownIcon />}
                     sx = {{
                         flexDirection: 'row-reverse', // Reverse the direction to move the icon to the left
-                        backgroundColor: '#ebf4f7'
+                        backgroundColor: '#ebf4f7',
+                        padding: '8px 16px', // Adjust padding as needed
                     }}
                 >
                     {title}
@@ -41,8 +42,6 @@ export function PracticeHelp({setOpen}: PracticeHelpProps){
                     sx = {{
                         flexDirection: 'row-reverse', // Reverse the direction to move the icon to the left
                         backgroundColor: '#ebf4f7',
-                        marginBottom: '0px',
-                        marginTop: '0px'
                     }}
                 >
                     {children}
@@ -69,60 +68,55 @@ export function PracticeHelp({setOpen}: PracticeHelpProps){
             </div>
 
             <div className={cn.content}>
+                {checkTab('func') &&
                 <div className={cn.accordions}>
-                    <CustomAccordion title={<h2>Mirror</h2>}>
-                        <div>
-                            <ul>
-                                <li>Click the Mirror button to flip the video</li>
-                            </ul>
-                        </div>
-                    </CustomAccordion>
+                        <CustomAccordion title={<h2>Video Player</h2>}>
+                            <div className={cn.function}>
+                                <ul>
+                                    <li>The 'Mirror' button will flip your video horizontally so you can learn whichever way feels best</li>
+                                    <li>The gear opens a pop up screen that contains all the features of this site</li>
+                                </ul>
+                                <img width={"40%"} src="images/mirror.png"></img>
 
-                    <CustomAccordion title={<h2>Settings</h2>}>
-                        <div>
-                            <ul>
-                                <li> Open the settings using the settings button to access all features below</li>
-                            </ul>
-                        </div>
-                    </CustomAccordion>
+                            </div>
+                        </CustomAccordion>
 
-                    <CustomAccordion title={<h2>Countdown</h2>}>
-                        <div> 
-                            <ul> 
-                                <li> Set the seconds counted before resuming the video </li>
-                                <li> Click the "Start Countdown" button to start the countdown </li>
-                            </ul>
-                        </div>
-                    </CustomAccordion>
+                        <CustomAccordion title={<h2>Countdown</h2>}>
+                            <div className={cn.function}>
+                                <p>Set the countdown time from 1 to 10 seconds and click 'Start Countdown' to immediately
+                                    start counting down.</p> 
+                                <img width={"40%"} src="images/countdown.png"></img>
 
-                    <CustomAccordion title={<h2>Speed</h2>}>
-                        <div> 
-                            <ul> 
-                                <li> Click on the speed you want to set the video to </li>
-                            </ul>
-                        </div>
-                    </CustomAccordion>
+                            </div>
+                        </CustomAccordion>
 
-                    <CustomAccordion title={<h2>Loop</h2>}>
-                        <div> 
-                            <ul> 
-                                <li>Skip the video to the timestamp you want to start the loop at</li>
-                                <li>Click the bookmark button next to "Start" to bookmark the timestamp</li>
-                                <li>Repeat with the end bookmark </li>
-                                <li>Check the box next to "Loop" to enable looping</li>
-                            </ul>
-                        </div>
-                    </CustomAccordion>
+                        <CustomAccordion title={<h2>Speed</h2>}>
+                            <div className={cn.function}> 
+                                <p>Clicking each number will change the video speed to that respective value.</p>
+                                <img width={"40%"} src="images/speed.png"></img>
+                            </div>
+                        </CustomAccordion>
 
-                    <CustomAccordion title={<h2>Skip Interval</h2>}>
-                        <div> 
-                            <ul> 
-                                <li>Drag the slider to determine how many seconds the arrow keys skip by</li>
-                            </ul>
-                        </div>
-                    </CustomAccordion>
+                        <CustomAccordion title={<h2>Loop</h2>}>
+                            <div className={cn.function}> 
+                                <p>Click the bookmark buttons while the video player timestamp is at the time you want to
+                                    start or end the the loop at and click the 'Start' or 'End' bookmarks. Once you
+                                    check the box next to 'Loop' your video will only play within those timestamps.
+                                </p>
+                                <img width={"40%"} src="images/loop.png"></img>
 
+                            </div>
+                        </CustomAccordion>
+
+                        <CustomAccordion title={<h2>Skip Interval</h2>}>
+                            <div className={cn.function}> 
+                                <p>Drag the slider to determine how many seconds the arrow keys skip the video by.</p>
+                                <img width={"40%"} src="images/skip.png"></img>
+                                
+                            </div>
+                        </CustomAccordion>
                 </div>
+                    }
             </div>
         </div>
        </div>
