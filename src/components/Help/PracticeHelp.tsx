1fc9eff26f8d8f0d9a4939546402579b594de202
Sarah <tans9@uci.edu>
2024-08-29 17:52:35 -0400

    changed style for help functions tab

--- src/components/Help/PracticeHelp.tsx
+++ src/components/Help/PracticeHelp.tsx
@@ -2,14 +2,54 @@ import cn from "./PracticeHelp.module.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { Dispatch, SetStateAction, useState } from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 type PracticeHelpProps = {
     setOpen: Dispatch<SetStateAction<boolean>>;
-
 }
 
 export function PracticeHelp({setOpen}: PracticeHelpProps){
     const [currTab, setCurrTab] = useState('func');
+
+    interface CustomAccordionProps {
+        title: React.ReactNode,
+        children: React.ReactNode
+    }
+
+    const CustomAccordion = ({title, children}: CustomAccordionProps) => {
+
+        return (
+            <Accordion 
+                disableGutters
+                sx = {{
+                    boxShadow: 'none'
+                }}
+            >
+                <AccordionSummary
+                    expandIcon={<ArrowDropDownIcon />}
+                    sx = {{
+                        flexDirection: 'row-reverse', // Reverse the direction to move the icon to the left
+                        backgroundColor: '#ebf4f7'
+                    }}
+                >
+                    {title}
+                </AccordionSummary>
+                <AccordionDetails
+                    sx = {{
+                        flexDirection: 'row-reverse', // Reverse the direction to move the icon to the left
+                        backgroundColor: '#ebf4f7',
+                        marginBottom: '0px',
+                        marginTop: '0px'
+                    }}
+                >
+                    {children}
+                </AccordionDetails>
+            </Accordion>
+        );
+    };
 
     const checkTab = (val: string) => {
         return currTab == val;
@@ -29,67 +69,60 @@ export function PracticeHelp({setOpen}: PracticeHelpProps){
             </div>
 
             <div className={cn.content}>
-                {checkTab('func') && 
-                <div className={cn.bulletPoints}>
-                    <div>
-                        <h3>Mirror</h3>
-                        <ul>
-                            <li> Click the Mirror button to flip the video</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>Settings</h3>
-                        <ul>
-                            <li> Open the settings using the settings button to access all features below</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>Countdown</h3>
-                        <ul> 
-                            <li> Set the seconds counted before resuming the video </li>
-                            <li> Click the "Start Countdown" button to start the countdown </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>Speed</h3>
-                        <ul>
-                            <li> Click on the speed you want to set the video to </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>Loop</h3>
-                        <ul>
-                            <li>Skip the video to the timestamp you want to start the loop at</li>
-                            <li>Click the bookmark button next to "Start" to bookmark the timestamp</li>
-                            <li>Repeat with the end bookmark </li>
-                            <li>Check the box next to "Loop" to enable looping</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3> Skip Interval</h3>
-                        <ul>
-                            <li>Drag the slider to determine how many seconds the arrow keys skip by</li>
-                        </ul>
-                    </div>
-                </div>
-                }
-                {checkTab('keybind') && 
-                <div>
-                    <div>
-                        <div className={cn.table}>
-                            <p>Left Arrow: Skip the video backwards the set seconds in settings</p>
-                            <p>Right Arrow: Skip the video forwards the set seconds in settings</p>
-                            <p>0: Skip back to the beginning of the video</p>
-                            <p>L: Skip the video forward by 10 seconds</p>
-                            <p>J: Skip the video backward by 10 seconds</p>
-                            <p>S: Open settings</p>
-                            <p>F: Enter or exit flil screen</p>
-                            <p>R: Mirror video</p>
-                            <p>M: Mute video</p>
+                <div className={cn.accordions}>
+                    <CustomAccordion title={<h2>Mirror</h2>}>
+                        <div>
+                            <ul>
+                                <li>Click the Mirror button to flip the video</li>
+                            </ul>
                         </div>
-                    </div>
+                    </CustomAccordion>
+
+                    <CustomAccordion title={<h2>Settings</h2>}>
+                        <div>
+                            <ul>
+                                <li> Open the settings using the settings button to access all features below</li>
+                            </ul>
+                        </div>
+                    </CustomAccordion>
+
+                    <CustomAccordion title={<h2>Countdown</h2>}>
+                        <div> 
+                            <ul> 
+                                <li> Set the seconds counted before resuming the video </li>
+                                <li> Click the "Start Countdown" button to start the countdown </li>
+                            </ul>
+                        </div>
+                    </CustomAccordion>
+
+                    <CustomAccordion title={<h2>Speed</h2>}>
+                        <div> 
+                            <ul> 
+                                <li> Click on the speed you want to set the video to </li>
+                            </ul>
+                        </div>
+                    </CustomAccordion>
+
+                    <CustomAccordion title={<h2>Loop</h2>}>
+                        <div> 
+                            <ul> 
+                                <li>Skip the video to the timestamp you want to start the loop at</li>
+                                <li>Click the bookmark button next to "Start" to bookmark the timestamp</li>
+                                <li>Repeat with the end bookmark </li>
+                                <li>Check the box next to "Loop" to enable looping</li>
+                            </ul>
+                        </div>
+                    </CustomAccordion>
+
+                    <CustomAccordion title={<h2>Skip Interval</h2>}>
+                        <div> 
+                            <ul> 
+                                <li>Drag the slider to determine how many seconds the arrow keys skip by</li>
+                            </ul>
+                        </div>
+                    </CustomAccordion>
+
                 </div>
-                }
             </div>
         </div>
        </div>
